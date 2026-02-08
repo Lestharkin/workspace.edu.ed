9616dc462ba0efc46028f35cbc9421778aa89789
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-02-08 18:13:00 -0500

    feat: add initial LinkedListTest class with test methods

---
+++ data_structure/src/test/java/edu/lestharkin/app/linkedlist/singly/LinkedListTest.java
@@ -0,0 +1,20 @@
+package edu.lestharkin.app.linkedlist.singly;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+  @Test
+  public void testAdd() {
+
+  }
+
+  @Test
+  public void testIsEmpty() {
+
+  }
+
+  @Test
+  public void testToString() {
+
+  }
+}

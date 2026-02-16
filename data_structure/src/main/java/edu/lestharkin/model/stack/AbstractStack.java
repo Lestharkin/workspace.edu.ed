56bc294353c7769f2946332a199ed50a8a43242a
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-02-16 06:16:40 -0500

    stack

---
+++ data_structure/src/main/java/edu/lestharkin/model/stack/AbstractStack.java
@@ -0,0 +1,16 @@
+package edu.lestharkin.model.stack;
+
+import edu.lestharkin.model.collection.AbstractCollection;
+
+/**
+ * A stack is a data structure that allows you to add and remove values from the
+ * top of the stack.
+ * This is a LIFO (Last In First Out) data structure.
+ * 
+ * @param <E> the type of value that the stack will hold.
+ * @author Lenin Javier Serrano Gil
+ * @version 1.0.20240219
+ */
+public abstract class AbstractStack<E> extends AbstractCollection<E> implements Stack<E> {
+
+}
